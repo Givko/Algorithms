6d76878a62daa1197f1db190f483f27bb4df6140
jivko.milev <milevjivko@gmail.com>
2022-01-16 21:38:02 +0200

    init commit

---
+++ AlgorithmsExercises/AlgorithmsExercises.Solutions/Fundamentals/RecursionAndBacktracking/FindAllPathsInLabyrinth.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsExercises.Solutions.Fundamentals.RecursionAndBacktracking
+{
+    public class FindAllPathsInLabyrinth
+    {
+        public static void FindAllPaths()
+        {
+            var rows = int.Parse(Console.ReadLine());
+            var cols = int.Parse(Console.ReadLine());
+
+            var labyrinth = new char[rows,cols];
+            for (int i = 0; i < rows; i++)
+            {
+                string rowElements = Console.ReadLine();
+                for (int j = 0; j < cols; j++)
+                {
+                    labyrinth[i,j] = rowElements[j];
+                }
+            }
+
+            FindAllPathsInternal(labyrinth, 0, 0, string.Empty, new Stack<string>());
+        }
+
+        private static void FindAllPathsInternal(char[,] labyrinth, int row, int col, string directionMoved, Stack<string> directionsMoved)
+        {
+            if (IsOutBoundary(labyrinth, row, col))
+            {
+                return;
+            }
+
+            if (labyrinth[row,col] == '*' || labyrinth[row, col] == 'x')
+            {
+                return;
+            }
+
+            directionsMoved.Push(directionMoved);
+            if (labyrinth[row,col] == 'e')
+            {
+                Console.WriteLine(string.Join(string.Empty, directionsMoved.Reverse()));
+                directionsMoved.Pop();
+                return;
+            }
+
+            labyrinth[row, col] = 'x';
+            FindAllPathsInternal(labyrinth, row, col + 1, "R", directionsMoved);
+            FindAllPathsInternal(labyrinth, row, col - 1, "L", directionsMoved);
+            FindAllPathsInternal(labyrinth, row + 1, col, "D", directionsMoved);
+            FindAllPathsInternal(labyrinth, row - 1, col, "U", directionsMoved);
+
+            labyrinth[row, col] = '-';
+            directionsMoved.Pop();
+        }
+
+        private static bool IsOutBoundary(char[,] labyrinth, int row, int col)
+        {
+            return row < 0 || col < 0 || row >= labyrinth.GetLength(0) || col >= labyrinth.GetLength(1);
+        }
+    }
+}
---
+++ AlgorithmsExercises/AlgorithmsExercises.Solutions/Fundamentals/RecursionAndBacktracking/QueenPuzzle.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsExercises.Solutions.Fundamentals.RecursionAndBacktracking
+{
+    public class QueenPuzzle
+    {
+        private static HashSet<int> attackedRows = new HashSet<int>();
+        private static HashSet<int> attackedCols = new HashSet<int>();
+        private static HashSet<int> attackedRightDiagonals = new HashSet<int>();
+        private static HashSet<int> attackedLeftDiagonals = new HashSet<int>();
+
+        public static void QueenSolutions()
+        {
+            var chessBoard = new char[8, 8]
+            {
+                { '-','-','-','-','-','-','-','-'},
+                { '-','-','-','-','-','-','-','-'},
+                { '-','-','-','-','-','-','-','-'},
+                { '-','-','-','-','-','-','-','-'},
+                { '-','-','-','-','-','-','-','-'},
+                { '-','-','-','-','-','-','-','-'},
+                { '-','-','-','-','-','-','-','-'},
+                { '-','-','-','-','-','-','-','-'}
+            };
+
+            PlaceQueen(chessBoard, 0);
+        }
+
+        private static void PlaceQueen(char[,] chessBoard, int row)
+        {
+            if (row >= chessBoard.GetLength(0))
+            {
+                Print(chessBoard);
+                return;
+            }
+
+            for (int j = 0; j < chessBoard.GetLength(1); j++)
+            {
+                if (IsSquareAttacked(row, j))
+                {
+                    continue;
+                }
+
+                chessBoard[row, j] = 'Q';
+                attackedRows.Add(row);
+                attackedCols.Add(j);
+                attackedLeftDiagonals.Add(row - j);
+                attackedRightDiagonals.Add(row + j);
+
+                PlaceQueen(chessBoard, row + 1);
+
+                chessBoard[row, j] = '-';
+                attackedRows.Remove(row);
+                attackedCols.Remove(j);
+                attackedLeftDiagonals.Remove(row - j);
+                attackedRightDiagonals.Remove(row + j);
+            }
+        }
+
+        private static void Print(char[,] chessBoard)
+        {
+            for (int i = 0; i < chessBoard.GetLength(0); i++)
+            {
+                for (int j = 0; j < chessBoard.GetLength(1); j++)
+                {
+                    Console.Write(chessBoard[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        private static bool IsSquareAttacked(int row, int col)
+        {
+            return attackedRows.Contains(row) ||
+                attackedCols.Contains(col) ||
+                attackedRightDiagonals.Contains(row + col) ||
+                attackedLeftDiagonals.Contains(row - col);
+        }
+
+        private static bool IsOutOfBoundary(char[,] chessBoard, int row, int col)
+        {
+            return row < 0 || col < 0 || row >= chessBoard.GetLength(0) || col >= chessBoard.GetLength(1);
+        }
+    }
+}
